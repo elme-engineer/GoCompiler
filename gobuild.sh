1e7d5b95286f52edc7629eb7234bec8f20928978
elme-engineer <pmcb.uc@gmail.com>
2024-12-14 15:58:50 +0000

    New build script

--- gobuild.sh
+++ gobuild.sh
@@ -2,4 +2,4 @@
 rm -f gocompiler lex.yy.c y.tab.c y.tab.h
 yacc -d -v -t -g -Wcounterexamples --report=all gocompiler.y
 lex gocompiler.l
-clang-14 -o gocompiler lex.yy.c y.tab.c goast.c -Wall -Wno-unused-function
+clang-14 -g -o gocompiler lex.yy.c y.tab.c goast.c
